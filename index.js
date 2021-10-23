da4b603a0ad1c6cce4bfdd0cf9acfa3dc8bdfeb3
realynsaldanha <78567724+realynsaldanha@users.noreply.github.com>
2021-10-23 15:27:58 +1300

    first commit for js

---
+++ index.js
@@ -0,0 +1,92 @@
+const questions = [
+    { 
+        "Question1": "Age range?", 
+        "answer1": "10-12",
+        "answer1Total": "1",
+        "answer2": "12-14",
+        "answer2Total": "2",
+        "answer3":"14-16",
+        "answer3Total": "3",
+        "answer4": "16-18",
+        "answer4Total": "4"
+    },
+    {
+        "Question2": "How often do you feel excessively worried?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+    }
+    {
+        "Question3":  "How often do you find it difficult to focus on schoolwork?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3": "Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+
+    }
+    { 
+        "Question4": "How often are you not in control or cannot stop worrying?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+
+    }
+    {
+        "Question5": "How often do you find it hard to relax?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+    }
+    {
+        "Question6": "How often do you find that you're restless because it is hard to sit still?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+    }
+    {
+        "Question7": "How often do you become easily annoyed or irritable?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+    }
+    {
+        "Question8": "How often do you feel afraid that something awful might happen?",
+        "answer1": "Never",
+        "answer1Total": "1",
+        "answer2": "Sometimes",
+        "answer2Total": "2",
+        "answer3":"Quite Often",
+        "answer3Total": "3",
+        "answer4": "All the time",
+        "answer4Total": "4"
+    }
+]
