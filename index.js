28a592654182201002123017784c835489d43f43
realynsaldanha <78567724+realynsaldanha@users.noreply.github.com>
2021-11-11 19:32:53 +1300

    Update index.js
    added: line 128 onwards

--- index.js
+++ index.js
@@ -106,3 +106,68 @@ const questions = [
   const restartButton = document.querySelector('.restart');
   const result = document.querySelector('.result');
 
+ function generateQuestions (index) {
+      const question = questions[index];
+      const option1Total = questions[index].answer1Total;
+      const option2Total = questions[index].answer2Total;
+      const option3Total = questions[index].answer3Total;
+      const option4Total = questions[index].answer4Total;
+      
+      questionEl.innerHTML = `${index + 1}. ${question.question}`
+      option1.setAttribute('data-total', `${option1Total}`);
+      option2.setAttribute('data-total', `${option2Total}`);
+      option3.setAttribute('data-total', `${option3Total}`);
+      option4.setAttribute('data-total', `${option4Total}`);
+      option1.innerHTML = `${question.answer1}`
+      option2.innerHTML = `${question.answer2}`
+      option3.innerHTML = `${question.answer3}`
+      option4.innerHTML = `${question.answer4}`
+  }
+  
+  
+  function loadNextQuestion () {
+      const selectedOption = document.querySelector('input[type="radio"]:checked');
+      
+      if(!selectedOption) {
+          alert('Please pick an answer');
+          return;
+      }
+     
+      const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
+  
+      
+      score.push(answerScore);
+  
+      selectedAnswersData.push()
+      
+  
+      const totalScore = score.reduce((total, currentNum) => total + currentNum);
+  
+      
+      currentQuestion++;
+  
+          
+          selectedOption.checked = false;
+      
+      if(currentQuestion == totalQuestions - 1) {
+          nextButton.textContent = 'Finish';
+      }
+      
+      if(currentQuestion == totalQuestions) {
+          container.style.display = 'none';
+          result.innerHTML =
+           `<h1 class="final-score">Your score: ${totalScore}</h1>
+              <div class="summary">
+              <h1>Summary of your results</h1>
+              <p>Match your score to these assesments below:</p>
+              <p>24 - 32- Severe Anxiety</p>
+              <p>17 - 23 - Moderate Anxiety</p>
+              <p>8 - 16 - Mild Anxiety</p>
+            
+          </div>
+      }
+      generateQuestions(currentQuestion);
+      
+  generateQuestions(currentQuestion);
+  nextButton.addEventListener('click', loadNextQuestion);
+
